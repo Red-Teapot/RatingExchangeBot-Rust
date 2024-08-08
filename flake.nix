31a2e115549b6faa62dee7cefdde93a28b7b6eb7
Nikita Medvedev <some.redteapot@gmail.com>
2024-08-08 18:37:14 +0400

    Implement building the app via Nix

--- flake.nix
+++ flake.nix
@@ -5,42 +5,47 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, naersk, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = pkgs.rust-bin.stable.latest.default;
-          rustc = pkgs.rust-bin.stable.latest.default;
+        toolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
         };
+        nativeBuildInputs = [
+          pkgs.pkg-config
+          pkgs.openssl.dev
+        ];
+        src = pkgs.lib.cleanSource ./.;
       in {
-        packages.default = rustPlatform.buildRustPackage {
+        packages.default = naersk'.buildPackage {
           pname = "rating-exchange-bot";
           version = "0.1.0";
-          cargoLock.lockFile = ./Cargo.lock;
-          src = pkgs.lib.cleanSource ./.;
+          src = src;
 
-          nativeBuildInputs = [
-            pkgs.openssl
-            pkgs.pkg-config
-          ];
+          nativeBuildInputs = nativeBuildInputs ++ (with pkgs; [ sqlx-cli]);
 
           PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+          preBuild = /* bash */ ''
+            export DATABASE_URL="sqlite:$(mktemp -d)/rebot-build.sqlite?mode=rwc"
+            sqlx database setup --source "${src}/migrations"
+          '';
         };
       
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = nativeBuildInputs ++ [ toolchain ];
+          
+          buildInputs = with pkgs; [
             lldb
-            (rust-bin.stable.latest.default.override {
-              extensions = [ "rust-analyzer" "rust-src" ];
-            })
             sqlx-cli
           ];
         };
